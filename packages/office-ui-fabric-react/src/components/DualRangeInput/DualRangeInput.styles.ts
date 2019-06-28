a3dd7d1a9aa318b93844191f9f3eccc8bb03fd6a
Brandon Thomas <brandth@microsoft.com>
2019-06-27 17:58:56 -0700

    style optimizations and updates

--- packages/office-ui-fabric-react/src/components/DualRangeInput/DualRangeInput.styles.ts
+++ packages/office-ui-fabric-react/src/components/DualRangeInput/DualRangeInput.styles.ts
@@ -1,10 +1,42 @@
 import { IDualRangeInputStyleProps, IDualRangeInputStyles } from './DualRangeInput.types';
-import { getFocusStyle } from '../../Styling';
+import { getFocusStyle, IStyle } from '../../Styling';
 
 const sliderHeight = 4;
 
 export const getDualRangeInputStyles = (props: IDualRangeInputStyleProps): IDualRangeInputStyles => {
   const { theme, className, enableTransitions } = props;
+
+  const thumbStyle: IStyle = [
+    getFocusStyle(theme, { isFocusedOnly: false, inset: -4 }),
+    {
+      borderWidth: 2,
+      borderStyle: 'solid',
+      borderColor: theme.palette.neutralSecondary,
+      borderRadius: 10,
+      boxSizing: 'border-box',
+      background: theme.palette.white,
+      display: 'block',
+      width: 16,
+      height: 16,
+      transform: 'translate(-50%,-50%)',
+      marginTop: 2,
+      selectors: {
+        '::after': {
+          display: 'none'
+        },
+        '*[dir="rtl"] &': {
+          transform: 'translate(50%,-50%)'
+        }
+      }
+    }
+  ];
+  const rangeStyle: IStyle = {
+    position: 'absolute',
+    pointerEvents: 'none',
+    height: 0,
+    width: 0,
+    opacity: 0
+  };
   return {
     root: [
       { display: 'flex', position: 'relative', height: 28, alignItems: 'center', userSelect: 'none' },
@@ -18,74 +50,26 @@ export const getDualRangeInputStyles = (props: IDualRangeInputStyleProps): IDual
       ...(enableTransitions && { transition: 'flex-basis 500ms ease-in-out' })
     },
     startContainer: { height: sliderHeight, flex: '0 0 0px', maxWidth: 0 },
-    startRange: {
-      position: 'absolute',
-      pointerEvents: 'none',
-      height: 0,
-      width: 0,
-      opacity: 0,
-      selectors: {
-        '&:focus + $startThumb:after': { display: 'block' }
-      }
-    },
-    startThumb: [
-      getFocusStyle(theme, { isFocusedOnly: false }),
+    startRange: [
+      rangeStyle,
       {
-        borderWidth: 2,
-        borderStyle: 'solid',
-        borderColor: theme.palette.neutralSecondary,
-        borderRadius: 10,
-        boxSizing: 'border-box',
-        background: theme.palette.white,
-        display: 'block',
-        width: 16,
-        height: 16,
-        transform: 'translate(-50%,-50%)',
-        marginTop: 2,
         selectors: {
-          '::after': { display: 'none' },
-          '*[dir="rtl"] &': {
-            transform: 'translate(50%,-50%)'
-          }
+          '&:focus + $startThumb:after': { display: 'block' }
         }
       }
     ],
+    startThumb: thumbStyle,
     filler: { height: sliderHeight, flex: '1 1 auto', backgroundColor: theme.palette.neutralSecondary },
     endContainer: { height: sliderHeight, flex: '0 0 0px', maxWidth: 0 },
-    endRange: {
-      position: 'absolute',
-      pointerEvents: 'none',
-      height: 0,
-      width: 0,
-      opacity: 0,
-      selectors: {
-        '&:focus + $endThumb:after': { display: 'block' }
-      }
-    },
-    endThumb: [
-      getFocusStyle(theme, { isFocusedOnly: false }),
+    endRange: [
+      rangeStyle,
       {
-        borderWidth: 2,
-        borderStyle: 'solid',
-        borderColor: theme.palette.neutralSecondary,
-        borderRadius: 10,
-        boxSizing: 'border-box',
-        background: theme.palette.white,
-        display: 'block',
-        width: 16,
-        height: 16,
-        transform: 'translate(-50%,-50%)',
-        marginTop: 2,
         selectors: {
-          '::after': {
-            display: 'none'
-          },
-          '*[dir="rtl"] &': {
-            transform: 'translate(50%,-50%)'
-          }
+          '&:focus + $endThumb:after': { display: 'block' }
         }
       }
     ],
+    endThumb: thumbStyle,
     endDeadSpace: {
       height: sliderHeight,
       borderRadius: '0px 4px 4px 0px',
